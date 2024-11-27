0c41458610ff390d24ba727229ac319d0e0349f9
levannamtdh <nam19982020@gmail.com>
2024-11-27 20:48:46 +0700

    Add files via upload

---
+++ Bài 2/2.01.py
@@ -0,0 +1,13 @@
+print("Sinh vien: Le Van Nam")
+print("MSSV: 235752021610085")
+print("################################")
+#########################################
+
+
+n1=int(input("enter n1 value: "))
+n2=int(input("enter n2 value: "))
+if n1 > n2:
+   print("n1 is big")
+else:
+   print("n2 is big")
+    
