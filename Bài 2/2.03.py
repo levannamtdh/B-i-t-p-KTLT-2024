c7c05b67a0d89b80efbc8b16391dd36c4a1b37b2
levannamtdh <nam19982020@gmail.com>
2024-11-27 20:52:27 +0700

    Add files via upload

---
+++ Bài 2/2.03.py
@@ -0,0 +1,10 @@
+print("Sinh vien: Le Van Nam")
+print("MSSV: 235752021610085")
+print("################################")
+#########################################
+
+n=int(input("enter a number------>"))
+if n & 2 == 0:
+            print("EVEN number");
+else:
+            print("ODD number");
