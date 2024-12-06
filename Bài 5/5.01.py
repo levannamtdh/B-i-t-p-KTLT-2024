43551c0a777dd9c2a677843fbdfdb6bfdbd0af93
levannamtdh <nam19982020@gmail.com>
2024-12-06 13:49:20 +0700

    Add files via upload

---
+++ Bài 5/5.01.py
@@ -0,0 +1,21 @@
+print("Sinh vien: Le Van Nam")
+print("MSSV: 235752021610085")
+print("################################")
+#########################################
+
+## My script using the math module ##
+import mymath #Note no.py
+
+values = [2,4,6,8,10]
+print('Squares:')
+for v in values:
+    print(mymath.spuare(v))
+print('Cubes:')
+for v in values:
+    print(mymath.cube(v))
+    
+print('Average: ' + str(mymath.average(values)))
+
+
+
+          
